4b08718672fdef937a97dc3a0e5b595938756ea7
ccscbsjhcvkasb <142353294+ccscbsjhcvkasb@users.noreply.github.com>
2023-08-16 04:53:31 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '64c4f654eb75b327721f9164';
+_mrmcp['creative_id'] = '64d50a00abc90752fc28204a';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 626;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '别踩白块60帧1';
+_mrmcp['build_number'] = parseInt('1192');
+_mrmcp['publish_time'] = 'Wed Aug 16 2023 02:33:07 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
